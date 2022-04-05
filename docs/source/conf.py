84eef3edc51fb8f8b79737ea7cd7bd2f5c3e5688
Anthony Scott <ascott329@gmail.com>
2022-04-05 19:48:19 +0000

    updated docs version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,12 +18,12 @@ sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 
 project = 'CluBCpG'
-copyright = '2020, Anthony Scott, PhD'
+copyright = '2022, Anthony Scott, PhD'
 author = 'Anthony Scott, PhD'
 
-version = '0.2.4'
+version = '0.2.5'
 # The full version, including alpha/beta/rc tags
-release = '0.2.4'
+release = '0.2.5'
 
 
 # -- General configuration ---------------------------------------------------
